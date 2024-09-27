cb6dab07960ad885ff99929bf245d99e8faf5f94
Danilo Maia Florenzano <daniloflorenzano1@outlook.com>
2024-09-27 19:00:30 -0300

    remove mapeamento desnecessario de News.cs

--- src/Core/News/News.cs
+++ src/Core/News/News.cs
@@ -5,28 +5,21 @@ namespace Core.News;
 
 public sealed class News
 {
-    [JsonProperty(PropertyName = "id")]
+    
     public Guid Id { get; set; } = Guid.NewGuid();
 
-    [JsonProperty(PropertyName = "title")]
     public string Title { get; set; } = string.Empty;
 
-    [JsonProperty(PropertyName = "content")]
     public string Content { get; set; } = string.Empty;
 
-    [JsonProperty(PropertyName = "author")]
     public string Author { get; set; } = string.Empty;
     
-    [JsonProperty(PropertyName = "subject")]
     public SubjectEnum Subject { get; set; }
 
-    [JsonProperty(PropertyName = "publishedAt")]
     public DateTime PublishedAt { get; set; }
 
-    [JsonProperty(PropertyName = "originalUrl")]
     public Uri OriginalUrl { get; set; } = null!;
 
-    [JsonProperty(PropertyName = "imageUrl")]
     public Uri? ImageUrl { get; set; } = null!;
 
     public override string ToString()
